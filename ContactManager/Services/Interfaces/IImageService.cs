d4de579c9bd384036f58cfd271400af4c020084f
Mansor Almossa <malmossa@gmail.com>
2024-10-11 08:26:29 -0700

    Completed

---
+++ ContactManager/Services/Interfaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactManager.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
