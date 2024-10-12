879bd5af7ee736033bc513ee88b907ca610b56cc
Mansor Almossa <malmossa@gmail.com>
2024-10-12 10:14:51 -0700

    completed

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -2,6 +2,8 @@ using ContactManager.Data;
 using ContactManager.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ContactManager.Services.Interfaces;
+using ContactManager.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+// Custom Services
+builder.Services.AddScoped<IImageService, ImageService>();
 
 var app = builder.Build();
 
