9e88207b05f5120477337a0fccb1ac67d88e6227
Mansor Almossa <malmossa@gmail.com>
2024-10-13 19:51:59 -0700

    completed

--- ContactManager/Services/AddressBookService.cs
+++ ContactManager/Services/AddressBookService.cs
@@ -13,9 +13,27 @@ namespace ContactManager.Services
         {
             _context = context;
         }
-        public Task AddContactToCategoryAsync(int categoryId, int contactId)
+        public async Task AddContactToCategoryAsync(int categoryId, int contactId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Check to see if the category is in the contact.
+                if (!await IsContactInCategory(categoryId, contactId))
+                {
+                    Contact? contact = await _context.Contacts.FindAsync(contactId);
+                    Category? category = await _context.Categories.FindAsync(categoryId);
+
+                    if (contact != null && category != null)
+                    {
+                        category.Contacts.Add(contact);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            } catch (Exception)
+            {
+                throw;
+            }
+       
         }
 
         public Task<ICollection<Category>> GetContactCategoriesAsync(int contactId)
@@ -45,9 +63,14 @@ namespace ContactManager.Services
             return categories;
         }
 
-        public Task<bool> IsContactInCategory(int categoryId, int contactId)
+        public async Task<bool> IsContactInCategory(int categoryId, int contactId)
         {
-            throw new NotImplementedException();
+            Contact? contact = await _context.Contacts.FindAsync(contactId);
+
+            return await _context.Categories
+                                 .Include(c => c.Contacts)
+                                 .Where(c => c.Id == categoryId && c.Contacts.Contains(contact))
+                                 .AnyAsync();
         }
 
         public Task RemoveContactFromCategoryAsync(int categoryId, int contactId)
